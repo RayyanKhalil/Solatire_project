dcb95f46ef54fc159f86f8b62bd56f158e4bb4c9
dell <dell@DESKTOP-V1BV01O>
2023-10-06 00:33:33 +0500

    test1

--- Solatire_bscs22149_bscs22107/Utility.h
+++ Solatire_bscs22149_bscs22107/Utility.h
@@ -1,1 +1,28 @@
 #pragma once
+#pragma once
+#pragma once
+#include"Card.h"
+
+//////////////////////////ye puri class me krooga 
+enum class Card_Types
+{
+    CLUBS = 1, DIAMONDS, HEARTS, SPADES
+};
+
+
+enum class cardNames
+{
+    ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+};
+
+enum class Color_
+{
+    RED, BLACK
+};
+
+
+enum class card_Row
+{
+    Waste_pile, SHUFFLED_PILE22, DRAW_PILE, N_1, N_2, N_3, N_4, N_5, N_6, N_7, R1, R2, R3, R4,
+};
+
