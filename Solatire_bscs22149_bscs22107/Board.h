dbfe5e919f7acb7c50c64de56c07bfc150f0edde
dell <dell@DESKTOP-V1BV01O>
2023-10-06 00:40:49 +0500

    test 03

--- Solatire_bscs22149_bscs22107/Board.h
+++ Solatire_bscs22149_bscs22107/Board.h
@@ -1,5 +1,14 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include<Windows.h>
+#include<windows.h>
+using namespace sf;
+
 class Board
 {
+public:
+    RectangleShape B;
+    CircleShape C;
+    Board();
+    Board(int s1, int s2);
 };
-
