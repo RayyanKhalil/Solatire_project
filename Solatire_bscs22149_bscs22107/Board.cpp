1e707d04236a0485b48369f704989df0897a108a
dell <dell@DESKTOP-V1BV01O>
2023-10-06 00:46:53 +0500

    test04

--- Solatire_bscs22149_bscs22107/Board.cpp
+++ Solatire_bscs22149_bscs22107/Board.cpp
@@ -1,1 +1,30 @@
 #include "Board.h"
+
+#include "Game.h"
+
+Board::Board()
+{
+    int y = 100;
+    int x = 100;
+    int ypos = 0; int xpos = 0;
+    B.setSize(Vector2f(y + 12, x + 58));
+    B.setPosition(Vector2f(ypos, xpos));
+    B.setFillColor(Color::Transparent);
+
+    int _col = 255;
+    B.setOutlineColor(Color(_col, _col, _col, _col - 128));
+    int s = 1;
+    B.setOutlineThickness(1);
+}
+Board::Board(int startXPosition, int startYPosition)
+{
+    int y = 100;
+    int x = 100;
+    B.setSize(Vector2f(y + 12, x + 58));
+    B.setPosition(Vector2f(startXPosition, startYPosition));
+    B.setFillColor(Color::Transparent);
+    int _col = 255;
+    B.setOutlineColor(Color(_col, _col, _col, _col - 128));
+    int s = 1;
+    B.setOutlineThickness(1);
+}
