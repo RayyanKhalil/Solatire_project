9a5e808dc48d351bcf9b806ce166d7b62de56ce4
dell <dell@DESKTOP-V1BV01O>
2023-10-06 00:54:48 +0500

    test05

--- Solatire_bscs22149_bscs22107/Card.cpp
+++ Solatire_bscs22149_bscs22107/Card.cpp
@@ -1,1 +1,60 @@
 #include "Card.h"
+#include"Utility.h"
+#include<algorithm>
+
+Card::Card()
+{
+    flipped = false;
+    front_side.loadFromFile("a.png");
+    back_side.loadFromFile("a.png");
+    card_sprite.setTexture(back_side);
+    _x = 0;
+    _y = 0;
+}
+Card::Card(const Card& c)
+    : _type(c._type),
+    name(c.name),
+    color(c.color),
+    Row(c.Row),
+    flipped(c.flipped),
+    front_side(c.front_side),
+    back_side(c.back_side),
+    card_sprite(c.card_sprite),
+    _x(c._x),
+    _y(c._y)
+{
+
+}
+Card& Card::operator=(const Card& c)
+{
+    _type = c._type;
+    name = c.name;
+    color = c.color;
+    Row = c.Row;
+    flipped = c.flipped;
+    front_side = c.front_side;
+    back_side = c.back_side;
+    card_sprite = c.card_sprite;
+    _x = c._x;
+    _y = c._y;
+    return *this;
+}
+void swap(Card& first, Card& second)
+{
+    using std::swap;
+    swap(first._type, second._type);
+    swap(first.name, second.name);
+    swap(first.color, second.color);
+    swap(first.Row, second.Row);
+    swap(first.flipped, second.flipped);
+    swap(first.front_side, second.front_side);
+    swap(first.back_side, second.back_side);
+    swap(first.card_sprite, second.card_sprite);
+    swap(first._x, second._x);
+    swap(first._y, second._y);
+}
+bool operator==(const Card& lhs, const Card& rhs)
+{
+    return lhs.color == rhs.color && lhs.name == rhs.name && lhs._type == rhs._type;
+}
+
--- Solatire_bscs22149_bscs22107/Card.h
+++ Solatire_bscs22149_bscs22107/Card.h
@@ -1,5 +1,62 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include<Windows.h>
+#include<windows.h>
+#include <iostream>
+#include <stack>
+#include <random>
+#include"Utility.h"
+
+
+using namespace sf;
+using namespace std;
 class Card
 {
+public:
+    Card();
+    Card(const Card&);
+    Card& operator=(const Card&);
+    friend bool operator==(const Card&, const Card&);
+    Card_Types _type;
+    cardNames name;
+    Color_ color, c2;
+    card_Row Row, r5;
+    bool flipped;
+    Texture front_side;
+    Texture back_side;
+    Sprite card_sprite, dummy_sprite;
+    float _x, _y;
+};
+
+
+
+
+
+
+
+#define stringify( name ) #name
+static string convert_suits[] =
+{
+        stringify(CLUBS),
+        stringify(DIAMONDS),
+        stringify(HEARTS),
+        stringify(SPADES)
+};
+
+static string convert_names[] =
+{
+        stringify(ACE),
+        stringify(TWO),
+        stringify(THREE),
+        stringify(FOUR),
+        stringify(FIVE),
+        stringify(SIX),
+        stringify(SEVEN),
+        stringify(EIGHT),
+        stringify(NINE),
+        stringify(TEN),
+        stringify(JACK),
+        stringify(QUEEN),
+        stringify(KING)
 };
 
